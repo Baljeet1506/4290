d55f15fb5f07e7de284348711dec13678466e389
kaurb <baljeet.yadav@email.kpu.ca>
2022-06-21 18:55:35 -0700

    latest commit boyzzzz

---
+++ app/src/main/java/com/example/realtorandviewer/FindRealtor.java
@@ -0,0 +1,14 @@
+package com.example.realtorandviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindRealtor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_realtor);
+    }
+}
---
+++ app/src/main/java/com/example/realtorandviewer/UserHome.java
@@ -0,0 +1,59 @@
+package com.example.realtorandviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class UserHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_home);
+
+        Button logoutBtn = findViewById(R.id.btnLogout);
+        Button findRealtorBtn = findViewById(R.id.findRealtorBtn);
+        Button recommendedRealtorsBtn = findViewById(R.id.recommendedRealtorBtn);
+        Button favoriteBtn = findViewById(R.id.favoritePageBtn);
+        Button mortgageCalBtn = findViewById(R.id.mortgageCalculatorBtn);
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+        findRealtorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), FindRealtor.class));
+            }
+        });
+
+        recommendedRealtorsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RecommendedRealtor.class));
+            }
+        });
+
+        favoriteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), FavoritePage.class));
+            }
+        });
+
+        mortgageCalBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MortgageCalculator.class));
+            }
+        });
+
+    }
+}
