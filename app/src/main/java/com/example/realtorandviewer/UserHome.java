62af112987cc34c3367239fe580b7ee086c9bc59
schopengaard <j.schopengaard@gmail.com>
2022-06-24 17:49:46 -0700

    Some front end changes

--- app/src/main/java/com/example/realtorandviewer/UserHome.java
+++ app/src/main/java/com/example/realtorandviewer/UserHome.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class UserHome extends AppCompatActivity {
 
@@ -14,38 +15,15 @@ public class UserHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
 
-        Button logoutBtn = findViewById(R.id.btnLogout);
-        Button findRealtorBtn = findViewById(R.id.findRealtorBtn);
-        Button recommendedRealtorsBtn = findViewById(R.id.recommendedRealtorBtn);
-        Button favoriteBtn = findViewById(R.id.favoritePageBtn);
-        Button mortgageCalBtn = findViewById(R.id.mortgageCalculatorBtn);
-        Button findPropertiesBtn = findViewById(R.id.findPropertiesBtn);
-
-        logoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            }
-        });
+        ImageButton findRealtorBtn = findViewById(R.id.findRealtorBtn);
+        ImageButton mortgageCalBtn = findViewById(R.id.mortgageCalculatorBtn);
+        ImageButton findPropertiesBtn = findViewById(R.id.findPropertiesBtn);
+        ImageButton profileBtn = findViewById(R.id.profileBtn);
 
         findRealtorBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), FindRealtor.class));
-            }
-        });
-
-        recommendedRealtorsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), RecommendedRealtor.class));
-            }
-        });
-
-        favoriteBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), FavoritePage.class));
             }
         });
 
