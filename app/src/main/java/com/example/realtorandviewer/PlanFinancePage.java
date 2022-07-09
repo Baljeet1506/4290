60a82daed0ae6d92643593c2634fd1061b41672e
ojesvee chandel <ojesveechandel54@gmail.com>
2022-07-08 19:42:26 -0700

    Made Ui changed to the Purchases page and finance page in Resources for viewers.

--- app/src/main/java/com/example/realtorandviewer/PlanFinancePage.java
+++ app/src/main/java/com/example/realtorandviewer/PlanFinancePage.java
@@ -2,7 +2,10 @@ package com.example.realtorandviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PlanFinancePage extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class PlanFinancePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_finance_page);
+        Button backButton = findViewById(R.id.backButton);
+        Button homeButton = findViewById(R.id.backButton1);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ResourcePageUser.class));
+            }
+        });
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), UserHome.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/realtorandviewer/PurchasedPage.java
+++ app/src/main/java/com/example/realtorandviewer/PurchasedPage.java
@@ -2,7 +2,10 @@ package com.example.realtorandviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PurchasedPage extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class PurchasedPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchased_page);
+
+        Button backButton = findViewById(R.id.backButton);
+        Button homeButton = findViewById(R.id.backButton1);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ResourcePageUser.class));
+            }
+        });
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), UserHome.class));
+            }
+        });
     }
 }
