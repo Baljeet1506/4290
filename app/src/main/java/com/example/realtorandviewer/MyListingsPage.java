24e34661a0322d4fc8e9cce60a02e5b86927983b
schopengaard <j.schopengaard@gmail.com>
2022-07-10 18:01:47 -0700

    Front end changes 0.6 - resource pages for both viewer and realtors

--- app/src/main/java/com/example/realtorandviewer/MyListingsPage.java
+++ app/src/main/java/com/example/realtorandviewer/MyListingsPage.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -17,7 +18,36 @@ public class MyListingsPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_listings_page);
 
+        ImageButton findRealtorBtn = findViewById(R.id.findRealtorBtn);
+        ImageButton mortgageCalBtn = findViewById(R.id.mortgageCalculatorBtn);
+        ImageButton findPropertiesBtn = findViewById(R.id.findPropertiesBtn);
+        ImageButton profileBtn = findViewById(R.id.profileBtn);
         FloatingActionButton listingsBtn = findViewById(R.id.listingsBtn);
+
+        findRealtorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), FindRealtor.class));
+            }
+        });
+        findPropertiesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), FindProperties.class));
+            }
+        });
+        mortgageCalBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MortgageCalculator.class));
+            }
+        });
+        profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), editProfile.class));
+            }
+        });
 
         listingsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/realtorandviewer/ResourcesPage.java
+++ app/src/main/java/com/example/realtorandviewer/ResourcesPage.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ResourcesPage extends AppCompatActivity {
 
@@ -14,11 +15,40 @@ public class ResourcesPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources_page);
 
+        ImageButton findRealtorBtn = findViewById(R.id.findRealtorBtn);
+        ImageButton mortgageCalBtn = findViewById(R.id.mortgageCalculatorBtn);
+        ImageButton findPropertiesBtn = findViewById(R.id.findPropertiesBtn);
+        ImageButton profileBtn = findViewById(R.id.profileBtn);
+
         Button PreparingBtn = findViewById(R.id.Preparing);
         Button ListingBtn = findViewById(R.id.Listing);
         Button MarketingBtn = findViewById(R.id.Marketing);
         Button OfferBtn = findViewById(R.id.Offer);
 
+        findRealtorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), FindRealtor.class));
+            }
+        });
+        findPropertiesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), FindProperties.class));
+            }
+        });
+        mortgageCalBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MortgageCalculator.class));
+            }
+        });
+        profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), UserHome.class));
+            }
+        });
 
        PreparingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
