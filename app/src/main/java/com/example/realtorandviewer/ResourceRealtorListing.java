c4aaca3ec508b42f2fba10306085134fac956355
schopengaard <j.schopengaard@gmail.com>
2022-07-13 14:14:44 -0700

    Front end changes 0.8 - Naming convention changes - back button progress - added things to activity_viewer_recommended_realtor.xml

--- app/src/main/java/com/example/realtorandviewer/ResourceRealtorListing.java
+++ app/src/main/java/com/example/realtorandviewer/ResourceRealtorListing.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MarketingPage extends AppCompatActivity {
+public class ResourceRealtorListing extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_marketing_page);
+        setContentView(R.layout.activity_resource_realtor_listing);
     }
 }
--- app/src/main/java/com/example/realtorandviewer/ResourceRealtorMarketing.java
+++ app/src/main/java/com/example/realtorandviewer/ResourceRealtorMarketing.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MakeAnOfferPage extends AppCompatActivity {
+public class ResourceRealtorMarketing extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_make_an_offer_page);
+        setContentView(R.layout.activity_resource_realtor_marketing);
     }
 }
