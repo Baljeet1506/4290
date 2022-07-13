4602f8b09fc3d59353025030806b254b37f66b07
schopengaard <j.schopengaard@gmail.com>
2022-07-13 14:15:02 -0700

    Front end changes 0.8 - Naming convention changes - back button progress - added things to activity_viewer_recommended_realtor.xml

---
+++ app/src/main/java/com/example/realtorandviewer/ResourceViewerMakeAnOffer.java
@@ -0,0 +1,29 @@
+package com.example.realtorandviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class ResourceViewerMakeAnOffer extends AppCompatActivity {
+
+    ImageButton btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resource_viewer_make_an_offer);
+        btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RealtorListings.class));
+            }
+        });
+
+    }
+
+}
