a67eadcc990f216ae644a2d7ca1ba55d7d7c40a9
schopengaard <62966740+schopengaard@users.noreply.github.com>
2022-07-15 11:43:47 -0700

    back button fix 2.0

--- app/src/main/java/com/example/realtorandviewer/ViewerFindProps.java
+++ app/src/main/java/com/example/realtorandviewer/ViewerFindProps.java
@@ -1,14 +1,12 @@
 package com.example.realtorandviewer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
 
-public class ViewerFindProps extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class ViewerFindProps extends AppCompatActivity {
     ImageButton backBtn;
 
     @Override
@@ -17,12 +15,6 @@ public class ViewerFindProps extends AppCompatActivity {
         setContentView(R.layout.activity_viewer_find_props);
 
         backBtn = findViewById(R.id.btnBack);
-
-        backBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), HomePageViewer.class));
-            }
-        });
+        backBtn.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), HomePageViewer.class)));
     }
 }
