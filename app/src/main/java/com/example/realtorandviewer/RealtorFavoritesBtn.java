161f3f2b84db70be0acedf61aad95e47695d3211
schopengaard <62966740+schopengaard@users.noreply.github.com>
2022-07-15 11:41:23 -0700

    Front end and Code cleanup, second part

--- app/src/main/java/com/example/realtorandviewer/RealtorFavoritesBtn.java
+++ app/src/main/java/com/example/realtorandviewer/RealtorFavoritesBtn.java
@@ -1,26 +1,21 @@
 package com.example.realtorandviewer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class RealtorFavoritesBtn extends AppCompatActivity {
+
+    ImageButton backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_realtor_favorites_btn);
 
-        ImageButton backBtn = findViewById(R.id.favoriteBackBtn2);
-
-        backBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), HomePageRealtor.class));
-            }
-        });
+        backBtn = findViewById(R.id.btnBack);
+        backBtn.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), HomePageRealtor.class)));
     }
 }
